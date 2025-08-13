8d4eae01d809956a032905498d11c58fa2294350
Hamza Joosub <mojoosub1738@gmail.com>
2025-08-13 15:32:43 +0200

    d

--- WebApp.py
+++ WebApp.py
@@ -848,7 +848,7 @@ def portfolio_Attribution():
 
         #st.markdown("# Visualisation")
         
-        st.metric(label="Perf(%)", value=position_value['NAV'].values.tolist()[-1], delta=pnl_OT.iloc[-1,0])
+        st.metric(label="Perf(%)", value=f'R {round(position_value['NAV'].values.tolist()[-1],2)}', delta=round(pnl_OT.iloc[-1,0],2))
                   
                   
         col1, col2 = st.columns(2)
